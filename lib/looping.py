08511d7bf2388971e549fb557af81325cc627003
Christian Hernandez <chernandez148@DESKTOP-3F6UKNT.localdomain>
2023-03-17 19:58:16 -0700

    update

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+
+    for squared in int_list:
+        num_squared = squared * squared
+        squared_list.append(num_squared)
+    return squared_list
+
+square_integers([1, 2, 3, 4, 5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
